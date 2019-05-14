fec7b349f23ee07f44456fdeb0192eb6b5c7343f
macbook <macbook@172.21.42.29>
2019-05-14 14:55:57 +0200

    creato il design dell'applicazione, implementato scheletro pattern mvc, create classi per lo sviluppo della logica

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,52 @@
 package it.polito.tdp.spellchecker.controller;
 
+import it.polito.tdp.spellchecker.model.Dictionary;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
+
+
 public class SpellCheckerController {
-	
+	private Dictionary model;
+
+	public void setModel(Dictionary model) {
+		this.model = model;
+	}
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private MenuButton language;
+
+    @FXML
+    private TextArea inserimento;
+
+    @FXML
+    private TextArea risultato;
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert language != null : "fx:id=\"language\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert inserimento != null : "fx:id=\"inserimento\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert risultato != null : "fx:id=\"risultato\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+
+    }
 }
---
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/TestModel.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.spellchecker.model;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
